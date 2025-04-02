ea17a9779ec96bf25e7bf40887e3ab9c46bb6c7d
sawe-daisy <deesawe16@gmail.com>
2025-04-02 11:25:39 +0300

    bitcoin cli command to create 2-of-3 P2SH multisig

--- submission/04.sh
+++ submission/04.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
+pubkey1="024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d"
+pubkey2="035e11024a7fa1e0ca537e0e76b15c1631486f6611c7d8187aea8f6c1ae5d30617"
+pubkey3="03ce11c7b563c3d7fae916351f234eac5089ad2e42fa9e29a94ee944e00c880c66"
+
+redeem_script=$(bitcoin-cli -regtest createmultisig 2 "[\"$pubkey1\",\"$pubkey2\",\"$pubkey3\"]")
+
+address=$(echo $redeem_script | jq -r '.address')
+echo "$address"
+
+
 # Create a 2-of-3 P2SH multisig address from the public keys below:
 
 # 024bc198a88b69316952414af34105613c3617561a433a229100a0966cd631dd3d
