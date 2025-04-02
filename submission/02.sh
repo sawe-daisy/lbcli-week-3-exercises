2218fda9bfb5c2486e5375a1a9c1362ab7e8e69a
sawe-daisy <deesawe16@gmail.com>
2025-04-02 11:07:09 +0300

    bitcoin cli command to extract pubkey

--- submission/02.sh
+++ submission/02.sh
@@ -2,6 +2,5 @@
 # Create a native segwit address and get the public key from the address.
 
 ADDRESS=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getnewaddress "" "bech32")
-echo "Address: $ADDRESS"
 PUBKEY=$(bitcoin-cli -regtest -rpcwallet="btrustwallet" getaddressinfo "$ADDRESS" | jq -r '.pubkey')
 echo "$PUBKEY"
