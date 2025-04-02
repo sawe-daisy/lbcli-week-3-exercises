a578d3aa5b9beddea27ef8561b8187c8b302b5f0
sawe-daisy <deesawe16@gmail.com>
2025-04-02 13:32:50 +0300

    bitcoin cli command to Extract the receivers address

--- submission/06.sh
+++ submission/06.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 transaction="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
 hash=$(bitcoin-cli -regtest decodepsbt "$transaction" | jq -r '.tx.hash')
-echo "Hash: $hash"
+echo "$hash"
 
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,11 @@
+#!/bin/bash
+
+transaction="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+
+decoded_psbt=$(bitcoin-cli -regtest decodepsbt "$transaction")
+
+receiver_address=$(echo "$decoded_psbt" | jq -r '.tx.vout[0].scriptPubKey.address')
+echo "$receiver_address"
+
 # What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
